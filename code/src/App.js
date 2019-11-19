4e3d0bfa1d6bf02a72109c566522c725fadd9701
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-11-19 21:49:51 +0100

    First record showing check

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import data from './data.json'
 import { Album } from './Album'
+import { Header } from './Header'
 
 console.log(data)
 
 export const App = () => {
   return (
     <div>
+      <Header />
       <Album />
     </div>
   )
---
+++ code/src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const divStyle = {
+    color: 'black',
+    backgroundColor: 'black',
+    height: '100px',
+    width: '100%'
+}
+
+export const Header = () => {
+    return (
+        <div style={divStyle}>
+
+        </div>
+    )
+}
